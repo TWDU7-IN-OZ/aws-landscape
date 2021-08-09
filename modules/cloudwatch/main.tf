694a7a00df453ec17fbcdf62f4df2a6e902c3e6b
sasikumarganesantw <sasikumar.g@thoughtworks.com>
2021-08-09 13:49:21 +0530

    SD, SG, Updating the kafka utilisation alert for testing

--- modules/cloudwatch/main.tf
+++ modules/cloudwatch/main.tf
@@ -53,3 +53,28 @@ resource "aws_cloudwatch_metric_alarm" "Disk_Space_kafkaInstanceDisk" {
   alarm_actions             = ["arn:aws:cloudwatch:ap-southeast-1:483506802077:alarm:Kafka Disk Space Metrics"] #variablize it
   ok_actions                = ["arn:aws:cloudwatch:ap-southeast-1:483506802077:alarm:Kafka Disk Space Metrics"]
 }
+
+resource "aws_cloudwatch_metric_alarm" "Disk_Space_kafkaInstanceDiskTest"{
+    alarm_name = "KafkUtilisationAlarm_LaTest"
+    alarm_actions = ["arn:aws:sns:ap-southeast-1:483506802077:twdu7_CloudWatch_Alarms_Topic"]
+    ok_actions = []
+    actions_enabled = true
+    insufficient_data_actions = []
+    metric_name = "DiskSpaceUtilization"
+    namespace = "System/Linux"
+    statistic = "Average"
+
+    dimensions = {
+        MountPath = "/"
+        InstanceId = "i-01fdc617e6e5016e1"
+        Filesystem = "/dev/xvda1"
+    }
+
+    period = 300
+    evaluation_periods = 1
+    datapoints_to_alarm = 1
+    threshold = "40"
+    comparison_operator = "GreaterThanThreshold"
+    treat_missing_data = "missing"
+    }
+}
