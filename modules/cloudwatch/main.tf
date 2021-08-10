622a90a3aa092d3639f5dc7162590f12e4f9f045
ayush.sharma <ayush.sharma@thoughtworks.com>
2021-08-10 15:24:44 +0530

    #40 AC,AS: add alerts on EBS and add EBS monitoring in cloudwatch dashboard and add cloudwatch agent on Kafka EC2

--- modules/cloudwatch/main.tf
+++ modules/cloudwatch/main.tf
@@ -1,23 +1,35 @@
-resource "aws_cloudwatch_metric_alarm" "Disk_Space_kafkaInstanceDisk"{
-    alarm_name = "KafkUtilisationAlarm"
-    alarm_actions = ["arn:aws:sns:ap-southeast-1:483506802077:twdu7_CloudWatch_Alarms_Topic"]
-    ok_actions = []
-    actions_enabled = true
-    insufficient_data_actions = []
-    metric_name = "DiskSpaceUtilization"
-    namespace = "System/Linux"
-    statistic = "Average"
+data "aws_instance" "kafka_instance" {
+  filter {
+    name = "name"
+    values = [
+      "kafka-data-eng-twdu7-in-oz"]
+  }
+}
 
-    dimensions = {
-        MountPath = "/"
-        InstanceId = "i-01fdc617e6e5016e1"
-        Filesystem = "/dev/xvda1"
-    }
+resource "aws_cloudwatch_metric_alarm" "Disk_Space_kafkaInstanceDisk" {
+  alarm_name = "Kafka EBS Consumption over Threshold"
+  AlarmDescription = "Kafka EBS mount has utilised more space then the threshold"
+  alarm_actions = [
+    "arn:aws:sns:ap-southeast-1:483506802077:twdu7_CloudWatch_Alarms_Topic"]
+  ok_actions = []
+  actions_enabled = true
+  insufficient_data_actions = []
+  metric_name = "disk_used_percent"
+  namespace = "CWAgent"
+  statistic = "Average"
 
-    period = 300
-    evaluation_periods = 1
-    datapoints_to_alarm = 1
-    threshold = "70"
-    comparison_operator = "GreaterThanThreshold"
-    treat_missing_data = "missing"
+  dimensions = {
+    path = "/data"
+    InstanceId = data.aws_instance.kafka_instance.id
+    device = "xvdh"
+    fstype = "xfs"
+
+  }
+
+  period = 300
+  evaluation_periods = 1
+  datapoints_to_alarm = 1
+  threshold = "75"
+  comparison_operator = "GreaterThanThreshold"
+  treat_missing_data = "missing"
 }
---
+++ scripts/installCloudwatchAgentOnKafka.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+set -xe
+
+usage() {
+  echo "Install Cloudwatch agent on Kafka EC2"
+}
+
+if [ $# -eq 0 ]; then
+  usage
+  exit 1
+fi
+
+BASTION_PUBLIC_IP=$1
+TRAINING_COHORT=$2
+
+echo "====Updating SSH Config===="
+
+echo "
+	User ec2-user
+	IdentitiesOnly yes
+	ForwardAgent yes
+	DynamicForward 6789
+    StrictHostKeyChecking no
+    UserKnownHostsFile /dev/null
+
+Host emr-master.${TRAINING_COHORT}.training
+    User hadoop
+
+Host *.${TRAINING_COHORT}.training !bastion.${TRAINING_COHORT}.training
+	ForwardAgent yes
+	ProxyCommand ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ec2-user@${BASTION_PUBLIC_IP} -W %h:%p 2>/dev/null
+	User ec2-user
+    StrictHostKeyChecking no
+    UserKnownHostsFile /dev/null
+
+Host bastion.${TRAINING_COHORT}.training
+    User ec2-user
+    HostName ${BASTION_PUBLIC_IP}
+    DynamicForward 6789
+" >>~/.ssh/config
+
+echo "====Installing Cloudwatch agent on Kafka to monitor disk utilisation===="
+ssh kafka.${TRAINING_COHORT}.training <<EOF
+sudo yum -y install amazon-cloudwatch-agent
+sudo echo '
+{
+  "agent": {
+    "metrics_collection_interval": 60,
+    "run_as_user": "cwagent"
+  },
+  "metrics": {
+    "append_dimensions": {
+        "InstanceId": "${aws:InstanceId}"
+    },
+    "metrics_collected": {
+      "disk": {
+        "measurement": [
+          "used_percent"
+        ],
+        "metrics_collection_interval": 60,
+        "resources": [
+          "/data",
+          "/"
+        ]
+      }
+    }
+  }
+}
+' >> /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+sudo systemctl restart amazon-cloudwatch-agent
+EOF
+
+echo "====Installation of Cloudwatch agent done===="
