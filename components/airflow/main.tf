63e894f7ef296177200be058802d7220a6b5a6c4
shantanu.das <thoughtworks_shantanudas@mercedesme.onmicrosoft.com>
2021-08-13 13:29:37 +0530

    #35 SD, Remove public subnet

--- components/airflow/main.tf
+++ components/airflow/main.tf
@@ -11,7 +11,7 @@ data "terraform_remote_state" "trainingairflow" {
 module "airflow" {
   source = "../../modules/airflow"
   version           = "0.1.4"
-  
+
   ami               = "ami-6e28b517" # CentOS 7 community image for eu-west-1
   key               = "airflow-test"
   db_password       = "thisistheairflowdbpassword"
@@ -20,19 +20,20 @@ module "airflow" {
   vpc_id            = "${module.vpc.vpc_id}"
 
   security_group_id = "${aws_security_group.airflow-security-group.id}"
-  subnet_ids        = [ "${module.vpc.public_subnets[0]}", "${module.vpc.public_subnets[1]}" ]
+  # subnet_ids        = [ "${module.vpc.public_subnets[0]}", "${module.vpc.public_subnets[1]}" ]
+  subnet_ids = ["subnet-0067b49e65d5c24e8", "subnet-0b52b420abc514df3"]
 
 }
 
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+# module "vpc" {
+#   source = "terraform-aws-modules/vpc/aws"
 
-  name = "airflow-test-vpc"
-  cidr = "10.10.0.0/16"
+#   name = "airflow-test-vpc"
+#   cidr = "10.10.0.0/16"
 
-  azs             = ["eu-west-1a",     "eu-west-1b",     "eu-west-1c"    ]
-  private_subnets = ["10.10.1.0/24",   "10.10.2.0/24",   "10.10.3.0/24"  ]
-  public_subnets  = ["10.10.101.0/24", "10.10.102.0/24", "10.10.103.0/24"]
+#   azs             = ["eu-west-1a",     "eu-west-1b",     "eu-west-1c"    ]
+#   private_subnets = ["10.10.1.0/24",   "10.10.2.0/24",   "10.10.3.0/24"  ]
+#   public_subnets  = ["10.10.101.0/24", "10.10.102.0/24", "10.10.103.0/24"]
 
-  enable_dns_hostnames = true
-}
+#   enable_dns_hostnames = true
+# }
